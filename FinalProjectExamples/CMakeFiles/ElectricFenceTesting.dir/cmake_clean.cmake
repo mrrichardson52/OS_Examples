880dfec78d570cafdebdbec32a888ec7811b42a6
Matthew Richardson <matthew.r.richardson@vanderbilt.edu>
2016-12-04 17:36:52 +0100

    adding electric fence example

---
+++ FinalProjectExamples/CMakeFiles/ElectricFenceTesting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ElectricFenceTesting.dir/main.cpp.o"
+  "ElectricFenceTesting.pdb"
+  "ElectricFenceTesting"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ElectricFenceTesting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
